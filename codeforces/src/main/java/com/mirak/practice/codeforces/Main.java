3da54ac46df1f82af76daca60f61c6a327364246
Karim Masoud <masoud@Karims-MacBook-Pro-2.local>
2018-02-27 00:26:59 +0400

    Solve problem D, round 465.

--- codeforces/src/main/java/com/mirak/practice/codeforces/Main.java
+++ codeforces/src/main/java/com/mirak/practice/codeforces/Main.java
@@ -1,5 +1,19 @@
 package com.mirak.practice.codeforces;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class Main {
+  public static void main(String[] args){
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    int n = 1000000007;
+    for(int i = 2; i*i <= n; i++){
+      if(n %i == 0){
+        System.out.println("not prime");
+        return;
+      }
+    }
+    System.out.println("prime");
+
+  }
 }
---
+++ codeforces/src/main/java/com/mirak/practice/codeforces/rounds/Test.java
@@ -0,0 +1,5 @@
+package com.mirak.practice.codeforces.rounds;
+
+public class Test {
+
+}
