8ede51675ae84b0598857d95e8f06263162c8885
Karim Masoud <karim.masoud94@gmail.com>
2018-05-26 20:22:21 +0400

    Solve Add Bold Tag in String problem.

---
+++ Leetcode/src/com/mirak/leetcode/contests/individual/medium/AddBoldTagInString.java
@@ -0,0 +1,37 @@
+package com.mirak.leetcode.contests.individual.medium;
+
+public class AddBoldTagInString {
+  public String addBoldTag(String s, String[] dict) {
+
+    int[] mark = new int[s.length() + 2];
+
+    for(String str: dict) {
+      int idx = s.indexOf(str);
+      while(idx != -1) {
+        mark[idx] += 1;
+        mark[idx + str.length()] -= 1;
+        idx = s.indexOf(str, idx + 1);
+      }
+    }
+
+    for(int i = 1; i < mark.length; i++) {
+      mark[i] += mark[i - 1];
+    }
+
+    StringBuilder builder = new StringBuilder();
+    int idx = 0;
+    while(idx < s.length()) {
+      if(mark[idx] != 0) {
+        builder.append("<b>");
+        while(idx < s.length() && mark[idx] != 0) {
+          builder.append(s.charAt(idx++));
+        }
+        builder.append("</b>");
+      }else{
+        builder.append(s.charAt(idx++));
+      }
+    }
+    return builder.toString();
+  }
+}
+
