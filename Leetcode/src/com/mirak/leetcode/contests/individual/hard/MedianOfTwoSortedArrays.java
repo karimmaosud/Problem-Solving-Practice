13432f8dcc9f6ea5194dfbb7358bd2b007861ceb
Karim Masoud <karim.masoud94@gmail.com>
2018-06-02 13:55:42 +0400

    Solve median of two sorted arrays.

---
+++ Leetcode/src/com/mirak/leetcode/contests/individual/hard/MedianOfTwoSortedArrays.java
@@ -0,0 +1,80 @@
+package com.mirak.leetcode.contests.individual.hard;
+
+public class MedianOfTwoSortedArrays {
+
+  private static class Pair{
+    int idx1, idx2;
+    Pair(int idx1, int idx2) {
+      this.idx1 = idx1;
+      this.idx2 = idx2;
+    }
+  }
+
+  public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    int n = nums1.length;
+    int m = nums2.length;
+    int idx1 = (int) Math.ceil((n + m ) / 2.0);
+    double ret = getKthNumber(nums1, nums2, idx1);
+    if(((n + m) & 1) != 1) {
+      return (ret + getKthNumber(nums1, nums2, idx1 + 1)) / 2.0;
+    }
+    return ret;
+  }
+
+  private static int getKthNumber(int[] nums1, int[] nums2, int k) {
+
+    Pair pair = getRank(nums1, nums2, k);
+
+    if(pair.idx1 == -1) {
+      return nums2[k - 1];
+    }
+
+    int rank = pair.idx1 + pair.idx2 + 2;
+    if(rank < k) {
+      return nums2[k - pair.idx1 - 2];
+    }
+    return nums1[pair.idx1];
+  }
+
+
+  private static Pair getRank(int[] nums1, int[] nums2, int k) {
+    int low = 0;
+    int high = nums1.length - 1;
+    int highRank = -1;
+    while(low <= high) {
+      int mid = (low + high) / 2;
+      int rank = mid + 2 + getRankInArray(nums2, nums1[mid]);
+      if(rank < k) {
+        highRank = rank;
+        low = mid + 1;
+      }else if(rank > k){
+        high = mid - 1;
+      }else{
+        return new Pair(mid, rank - mid - 2);
+      }
+    }
+
+    return new Pair(high, highRank - high - 2);
+  }
+
+  private static int getRankInArray(int[] nums, int key) {
+    int low = 0;
+    int high = nums.length - 1;
+    while(low <= high) {
+      int mid = (low + high) / 2;
+      if(nums[mid] >= key) {
+        high = mid - 1;
+      }else {
+        low = mid + 1;
+      }
+    }
+    return high;
+  }
+
+  public static void main(String[] args) {
+    int[] nums1 = {1,3};
+    int[] nums2 = {2};
+
+    System.out.println(findMedianSortedArrays(nums1, nums2));
+  }
+}
