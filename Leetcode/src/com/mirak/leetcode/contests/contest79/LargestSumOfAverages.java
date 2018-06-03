41eb783db37ddf42cb1f5e25898c4e71f260feea
Karim Masoud <karim.masoud94@gmail.com>
2018-06-03 23:41:13 +0400

    Refactor Largest Sum of Averages problem.

--- Leetcode/src/com/mirak/leetcode/contests/contest79/LargestSumOfAverages.java
+++ Leetcode/src/com/mirak/leetcode/contests/contest79/LargestSumOfAverages.java
@@ -1,8 +1,9 @@
 package com.mirak.leetcode.contests.contest79;
 
 public class LargestSumOfAverages {
-  static double inf = 1000000000000000.0;
-  public static double largestSumOfAverages(int[] A, int K) {
+  private double inf = 1000000000000000.0;
+
+  public double largestSumOfAverages(int[] A, int K) {
     double[][] dp = new double[A.length][K + 1];
     int[] sum = new int[A.length];
     initDp(dp);
@@ -10,28 +11,28 @@ public class LargestSumOfAverages {
     return solve(0, A, sum, dp, K);
   }
 
-  private static void initDp(double[][]dp) {
-    for(int i = 0; i < dp.length; i++) {
-      for(int j = 0; j < dp[i].length; j++) {
+  private void initDp(double[][] dp) {
+    for (int i = 0; i < dp.length; i++) {
+      for (int j = 0; j < dp[i].length; j++) {
         dp[i][j] = -1.0;
       }
     }
   }
 
-  private static void initSum(int[] A, int[] sum) {
+  private void initSum(int[] A, int[] sum) {
     sum[0] = A[0];
-    for(int i = 1; i < A.length; i++) {
+    for (int i = 1; i < A.length; i++) {
       sum[i] = A[i] + sum[i - 1];
     }
   }
 
-  private static double solve(int idx, int[] A, int[] sum, double[][] dp, int k) {
+  private double solve(int idx, int[] A, int[] sum, double[][] dp, int k) {
 
-    if(k == 1) {
-      if(idx < A.length) {
+    if (k == 1) {
+      if (idx < A.length) {
         int n = A.length - idx;
         int rangeSum = sum[A.length - 1];
-        if(idx > 0) {
+        if (idx > 0) {
           rangeSum -= sum[idx - 1];
         }
         return (1.0 * rangeSum) / n;
@@ -39,18 +40,18 @@ public class LargestSumOfAverages {
       return -inf;
     }
 
-    if(idx == A.length) {
+    if (idx == A.length) {
       return -inf;
     }
 
-    if(dp[idx][k] != -1.0) {
+    if (dp[idx][k] != -1.0) {
       return dp[idx][k];
     }
 
     double ans = 0.0;
     int n;
-    int rangeSum = idx > 0? -sum[idx - 1]: 0;
-    for(int i = idx; i < A.length; i++) {
+    int rangeSum = idx > 0 ? -sum[idx - 1] : 0;
+    for (int i = idx; i < A.length; i++) {
       n = i - idx + 1;
       rangeSum += sum[i];
       double avg = (1.0 * rangeSum) / n;
@@ -58,10 +59,5 @@ public class LargestSumOfAverages {
       rangeSum -= sum[i];
     }
     return dp[idx][k] = ans;
-  }
-
-  public static void main(String[] args) {
-    int[] nums = {9,1,2,3,9};
-    System.out.println(largestSumOfAverages(nums, 3));
   }
 }
