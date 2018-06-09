d31fd59a2c65f1fa8e075e150fd2f3b9454958c9
Karim Masoud <karim.masoud94@gmail.com>
2018-06-09 15:40:19 +0400

    Solve A careercup problem.

--- CareerCup/src/com/mirak/careercup/palantir/SubsetsToSum.java
+++ CareerCup/src/com/mirak/careercup/palantir/SubsetsToSum.java
@@ -1,5 +1,39 @@
 package com.mirak.careercup.palantir;
 
-public class SubsetsToSum {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+public class SubsetsToSum {
+  public static List<LinkedList<Integer>> getSubsetsToSum(int[] nums, int sum) {
+    Arrays.sort(nums);
+    List<LinkedList<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> current = new LinkedList<>();
+    solve(nums, 0, current, res, sum);
+    return res;
+  }
+
+  private static void solve(int[] nums, int index, LinkedList<Integer> current, List<LinkedList<Integer>> res, int sum) {
+    if(sum == 0) {
+      res.add((LinkedList<Integer>) current.clone());
+      return;
+    }
+    if(sum < 0 || index == nums.length) {
+      return;
+    }
+    // take
+    current.add(nums[index]);
+    solve(nums, index + 1, current, res, sum - nums[index]);
+    current.removeLast();
+    int j = index;
+    while(j < nums.length && nums[j] == nums[index]) {
+      j++;
+    }
+    solve(nums, j, current, res, sum);
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1, 2, 2, 3, 4, 5};
+    System.out.println(getSubsetsToSum(nums, 5));
+  }
 }
