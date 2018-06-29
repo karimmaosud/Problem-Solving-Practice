2351a222f1fabb3b9324c113df9117a0b3642fa4
Karim Masoud <karim.masoud94@gmail.com>
2018-06-29 22:25:53 +0400

    Solve Largest Divisible Subset problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/LargestDivisibleSubset.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/LargestDivisibleSubset.java
@@ -1,5 +1,44 @@
 package com.mirak.leetcode.individual.medium;
 
-public class LargestDivisibleSubset {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
+public class LargestDivisibleSubset {
+  public List<Integer> largestDivisibleSubset(int[] nums) {
+    if(nums.length == 0) {
+      return new LinkedList<>();
+    }
+    Arrays.sort(nums);
+    int[] subsetSize = new  int[nums.length];
+    int[] parent = new int[nums.length];
+    for(int i = 0; i < subsetSize.length; i++) {
+      subsetSize[i] = 1;
+      parent[i] = i;
+    }
+    int maxSubsetLength = 1;
+    for(int i = nums.length - 1; i >= 0; i--) {
+      for(int j = i - 1; j >= 0; j--) {
+        if(nums[i] % nums[j] == 0 && subsetSize[j] < subsetSize[i] + 1) {
+          subsetSize[j] = subsetSize[i] + 1;
+          parent[j] = i;
+          maxSubsetLength = Math.max(maxSubsetLength, subsetSize[j]);
+        }
+      }
+    }
+    int lower = 0;
+    for(; lower < nums.length; lower++) {
+      if(subsetSize[lower] == maxSubsetLength) {
+        break;
+      }
+    }
+    List<Integer> list = new LinkedList<>();
+    list.add(nums[lower]);
+    while(parent[lower] != lower) {
+      lower = parent[lower];
+      list.add(nums[lower]);
+    }
+    return list;
+  }
 }
+
