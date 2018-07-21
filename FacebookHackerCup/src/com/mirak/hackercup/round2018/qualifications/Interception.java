71ff3f00d13d7de507d423422f80511019f95534
Karim Masoud <karim.masoud94@gmail.com>
2018-07-21 20:58:59 +0400

    Solve first two problems of facebook hackercup 2018 qualification round.

--- FacebookHackerCup/src/com/mirak/hackercup/round2018/qualifications/Interception.java
+++ FacebookHackerCup/src/com/mirak/hackercup/round2018/qualifications/Interception.java
@@ -1,5 +1,31 @@
 package com.mirak.hackercup.round2018.qualifications;
 
-public class Interception {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-}
+public class Interception {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("/Users/masoud/Desktop/interception.txt"));
+    BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/masoud/Desktop/out.txt"));
+    int T = Integer.parseInt(reader.readLine());
+    for(int t = 1; t <= T; t++) {
+      int N = Integer.parseInt(reader.readLine());
+      int[] p = new int[N + 1];
+      for(int i = 0; i < N + 1; i++) {
+        p[i] = Integer.parseInt(reader.readLine());
+      }
+      writer.write("Case #" + t + ": ");
+      if((N & 1) == 0) {
+        writer.write('0');
+      }else {
+        writer.write("1\n0.0");
+      }
+      writer.write('\n');
+    }
+    reader.close();
+    writer.close();
+  }
+ }
--- FacebookHackerCup/src/com/mirak/hackercup/round2018/qualifications/Tourist.java
+++ FacebookHackerCup/src/com/mirak/hackercup/round2018/qualifications/Tourist.java
@@ -1,5 +1,56 @@
 package com.mirak.hackercup.round2018.qualifications;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Tourist {
 
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("/Users/masoud/Desktop/tourist.txt"));
+    BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/masoud/Desktop/out.txt"));
+    int T = Integer.parseInt(reader.readLine());
+    String[] strs;
+    for (int t = 1; t <= T; t++) {
+      strs = reader.readLine().split(" ");
+      int n = Integer.parseInt(strs[0]);
+      int k = Integer.parseInt(strs[1]);
+      long v = Long.parseLong(strs[2]);
+
+      ArrayList<String> attractions = new ArrayList<>();
+      for (int i = 0; i < n; i++) {
+        attractions.add(reader.readLine());
+      }
+
+      ArrayList<Integer> cycleStart = new ArrayList<>();
+      cycleStart.add(0);
+      int next = k % n;
+      while (next != 0) {
+        cycleStart.add(next);
+        next = (next + k) % n;
+      }
+
+      int startIndex = cycleStart.get((int) ((v % cycleStart.size() - 1) + cycleStart
+          .size()) % cycleStart.size());
+      ArrayList<String> res = new ArrayList<>();
+      for (int i = 0; i < k - (n - startIndex); i++) {
+        res.add(attractions.get(i));
+      }
+
+      for (int i = startIndex; i < Math.min(n, startIndex + k); i++) {
+        res.add(attractions.get(i));
+      }
+      writer.write("Case #" + t + ":");
+      for (String str : res) {
+        writer.write(" " + str);
+      }
+      writer.write('\n');
+    }
+    reader.close();
+    writer.close();
+
+  }
 }
