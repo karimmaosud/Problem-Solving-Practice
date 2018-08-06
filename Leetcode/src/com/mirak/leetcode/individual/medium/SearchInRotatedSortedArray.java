f26007372bd79ef85c60d8625eff6598387ae1e2
Karim Masoud <karim.masoud94@gmail.com>
2018-08-06 21:58:05 +0400

    Refactor Search in Rotated Sorted Array problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/SearchInRotatedSortedArray.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/SearchInRotatedSortedArray.java
@@ -1,43 +1,39 @@
 package com.mirak.leetcode.individual.medium;
 
 public class SearchInRotatedSortedArray {
-  public static int search(int[] nums, int target) {
+
+  public int search(int[] nums, int target) {
     int low = 0;
     int high = nums.length - 1;
     while (low <= high) {
-      if(target == nums[low]) {
+      if (target == nums[low]) {
         return low;
       }
 
-      if(target == nums[high]) {
+      if (target == nums[high]) {
         return high;
       }
 
       int mid = (low + high) / 2;
 
-      if(nums[mid] == target) {
+      if (nums[mid] == target) {
         return mid;
       }
 
-      if(nums[mid] > nums[low]) {
-        if(target >= nums[low] && target < nums[mid]) {
+      if (nums[mid] > nums[low]) {
+        if (target >= nums[low] && target < nums[mid]) {
           high = mid - 1;
-        }else {
+        } else {
           low = mid + 1;
         }
-      }else {
-        if(target > nums[mid] && target <= nums[high]) {
+      } else {
+        if (target > nums[mid] && target <= nums[high]) {
           low = mid + 1;
-        }else {
+        } else {
           high = mid - 1;
         }
       }
     }
     return -1;
-  }
-
-  public static void main(String[] args) {
-    int[] nums = {5, 1, 2, 3, 4};
-    System.out.println(search(nums, 1));
   }
 }
