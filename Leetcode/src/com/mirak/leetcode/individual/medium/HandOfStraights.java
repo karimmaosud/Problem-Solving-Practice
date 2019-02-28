916767f826a1eb3237d2495ae5e074b90f26dea3
Karim Masoud <karim.masoud94@gmail.com>
2019-02-28 11:44:57 +0000

    Solve Hand of Straights problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/HandOfStraights.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/HandOfStraights.java
@@ -1,5 +1,31 @@
 package com.mirak.leetcode.individual.medium;
 
+import java.util.*;
+
 public class HandOfStraights {
+
+  public boolean isNStraightHand(int[] hand, int W) {
+    if (W == 0 || hand.length % W != 0) {
+      return false;
+    }
+    Arrays.sort(hand);
+    Map<Integer, Integer> count = new HashMap<>();
+    for (int num : hand) {
+      count.put(num, count.getOrDefault(num, 0) + 1);
+    }
+    for (int i = 0; i < hand.length; i++) {
+      if (count.get(hand[i]) == 0) {
+        continue;
+      }
+      for (int j = 0; j < W; j++) {
+        int next = hand[i] + j;
+        if (count.getOrDefault(next, 0) == 0) {
+          return false;
+        }
+        count.put(next, count.get(next) - 1);
+      }
+    }
+    return true;
+  }
 
 }
