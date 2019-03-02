4acb361dc2a355db705b4e1ec08a3771dd5f6a6c
Karim Masoud <karim.masoud94@gmail.com>
2019-03-02 23:39:14 +0000

    Solve Random Pick with Weight problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/RandomPickWithWeight.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/RandomPickWithWeight.java
@@ -1,5 +1,45 @@
 package com.mirak.leetcode.individual.medium;
 
+import java.util.*;
+
 public class RandomPickWithWeight {
 
+  class Solution {
+
+    private int[] sum;
+    private int[] w;
+    private Random random;
+
+    public Solution(int[] w) {
+      random = new Random();
+      this.w = w;
+      sum = new int[w.length];
+      sum[0] = w[0];
+      for (int i = 1; i < w.length; i++) {
+        sum[i] = w[i] + sum[i - 1];
+      }
+    }
+
+    public int pickIndex() {
+      int upperBound = sum[w.length - 1];
+      int rand = 1 + random.nextInt(upperBound);
+      return getWeightedIndex(rand);
+    }
+
+    private int getWeightedIndex(int key) {
+      int low = 0;
+      int high = sum.length - 1;
+      while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (sum[mid] < key) {
+          low = mid + 1;
+        } else if (sum[mid] > key) {
+          high = mid - 1;
+        } else {
+          return mid;
+        }
+      }
+      return low;
+    }
+  }
 }
