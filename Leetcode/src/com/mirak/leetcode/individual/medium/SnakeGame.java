e2add2bf9ef0d57d42001a5b535b2855be653e59
Karim Masoud <karim.masoud94@gmail.com>
2019-03-12 17:16:38 +0000

    Solve Design Snake Game problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/SnakeGame.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/SnakeGame.java
@@ -13,23 +13,24 @@ public class SnakeGame {
    * E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0].
    */
 
-  private int shift = 15;
-
+  private int score;
+  private LinkedList<int[]> snake;
+  private int foodCursor;
   private boolean[][] vis;
-  private LinkedList<Integer> snake;
-  private int foodPointer;
+  private int n, m;
   private int[][] food;
 
-  private int score;
-
   public SnakeGame(int width, int height, int[][] food) {
-    this.vis = new boolean[height][width];
-    this.vis[0][0] = true;
-    this.snake = new LinkedList<>();
-    this.snake.addLast(0);
-    this.foodPointer = 0;
+    score = 0;
+    snake = new LinkedList<>();
+    snake.addLast(new int[]{0, 0});
+    foodCursor = 0;
+    n = height;
+    m = width;
+    vis = new boolean[n][m];
+    vis[0][0] = true;
+
     this.food = food;
-    this.score = 0;
   }
 
   /**
@@ -39,83 +40,54 @@ public class SnakeGame {
    * @return The game's score after the move. Return -1 if game over.
    * Game over when snake crosses the screen boundary or bites its body.
    */
-
   public int move(String direction) {
-    int lastLocation = snake.getLast();
+    int[] nextCell = getNextCell(direction);
 
-    int currentRow = getRow(lastLocation);
-    int currentColumn = getColumn(lastLocation);
-
-    int nextRow = getNextRow(currentRow, direction);
-    int nextColumn = getNextColumn(currentColumn, direction);
-
-    if (isGameOver(nextRow, nextColumn)) {
+    if (isBorderCell(nextCell)) {
       return -1;
     }
 
-    int nextLocation = makeCell(nextRow, nextColumn);
-    if (isFoodCell(nextRow, nextColumn)) {
-      foodPointer++;
-      vis[nextRow][nextColumn] = true;
-      snake.addLast(nextLocation);
+    snake.addLast(nextCell);
+    vis[nextCell[0]][nextCell[1]] = true;
+
+    if (foodCell(nextCell)) {
+      foodCursor++;
       score++;
     } else {
-      moveSnake(nextLocation);
+      int[] tailCell = snake.removeFirst();
+      if (!(tailCell[0] == nextCell[0] && tailCell[1] == nextCell[1])) {
+        vis[tailCell[0]][tailCell[1]] = false;
+      }
     }
     return score;
   }
 
-  private int getNextRow(int currentRow, String direction) {
-    if (direction.equals("U")) {
-      return currentRow - 1;
-    } else if (direction.equals("D")) {
-      return currentRow + 1;
+  private int[] getNextCell(String direction) {
+    int[] snakeHead = snake.getLast();
+    switch (direction) {
+      case "U":
+        return new int[]{snakeHead[0] - 1, snakeHead[1]};
+      case "D":
+        return new int[]{snakeHead[0] + 1, snakeHead[1]};
+      case "R":
+        return new int[]{snakeHead[0], snakeHead[1] + 1};
+      default:
+        return new int[]{snakeHead[0], snakeHead[1] - 1};
     }
-    return currentRow;
   }
 
-  private int getNextColumn(int currentColumn, String direction) {
-    if (direction.equals("L")) {
-      return currentColumn - 1;
-    } else if (direction.equals("R")) {
-      return currentColumn + 1;
+  private boolean foodCell(int[] nextCell) {
+    return foodCursor < food.length && food[foodCursor][0] == nextCell[0]
+        && food[foodCursor][1] == nextCell[1];
+  }
+
+  private boolean isBorderCell(int[] nextCell) {
+    if (nextCell[0] < 0 || nextCell[1] < 0 || nextCell[0] == n || nextCell[1] == m) {
+      return true;
     }
-    return currentColumn;
-  }
-
-  private boolean isGameOver(int row, int column) {
-    return row < 0 || column < 0 || row == vis.length || column == vis[0].length
-        || (!isHeadCell(row, column) && vis[row][column]);
-  }
-
-  private boolean isHeadCell(int row, int column) {
-    int location = makeCell(row, column);
-    return snake.getFirst() == location;
-  }
-
-  private boolean isFoodCell(int row, int column) {
-    return foodPointer != food.length && food[foodPointer][0] == row
-        && food[foodPointer][1] == column;
-  }
-
-  private int makeCell(int row, int column) {
-    return row | (column << shift);
-  }
-
-  private int getRow(int location) {
-    return location & ((1 << shift) - 1);
-  }
-
-  private int getColumn(int location) {
-    return location >> shift;
-  }
-
-  private void moveSnake(int location) {
-    vis[getRow(location)][getColumn(location)] = true;
-    snake.addLast(location);
-    int deletedLocation = snake.removeFirst();
-    if (deletedLocation != location) {
-      vis[getRow(deletedLocation)][getColumn(deletedLocation)] = false;
+    if (vis[nextCell[0]][nextCell[1]]) {
+      return !(nextCell[0] == snake.getFirst()[0] && nextCell[1] == snake.getFirst()[1]);
     }
+    return false;
   }
 }
