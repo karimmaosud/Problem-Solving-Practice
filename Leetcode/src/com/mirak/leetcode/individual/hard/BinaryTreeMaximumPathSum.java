c1796efaac297d89f065e793a7b7a853c9f9751c
Karim Masoud <karim.masoud94@gmail.com>
2019-03-30 14:42:05 +0000

    Solve Binary Tree Maximum Path Sum problem.

--- Leetcode/src/com/mirak/leetcode/individual/hard/BinaryTreeMaximumPathSum.java
+++ Leetcode/src/com/mirak/leetcode/individual/hard/BinaryTreeMaximumPathSum.java
@@ -2,4 +2,40 @@ package com.mirak.leetcode.individual.hard;
 
 public class BinaryTreeMaximumPathSum {
 
+  class TreeNode {
+
+    TreeNode left, right;
+    int val;
+  }
+
+  private final int INF = 1000000000;
+  private int ans = Integer.MIN_VALUE;
+
+  public int maxPathSum(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    solve(root);
+    return ans;
+  }
+
+  private int solve(TreeNode node) {
+
+    if (node == null) {
+      return -INF;
+    }
+
+    int downLeft = solve(node.left);
+    int downRight = solve(node.right);
+
+    int nodeDownPath = node.val;
+
+    nodeDownPath = Math.max(nodeDownPath, node.val + downLeft);
+
+    nodeDownPath = Math.max(nodeDownPath, node.val + downRight);
+
+    ans = Math.max(ans, Math.max(nodeDownPath, node.val + downLeft + downRight));
+
+    return nodeDownPath;
+  }
 }
