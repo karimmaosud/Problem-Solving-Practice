269578db016ccc4ed2e4ae53eb1a3d80be8166eb
Karim Masoud <karim.masoud94@gmail.com>
2019-03-30 14:41:19 +0000

    Solve Maximum Product Subarray problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/MaximumProductSubarray.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/MaximumProductSubarray.java
@@ -2,4 +2,29 @@ package com.mirak.leetcode.individual.medium;
 
 public class MaximumProductSubarray {
 
+  public int maxProduct(int[] nums) {
+    long minNeg = 0, maxPos = 0, ans = 0;
+    if (nums.length == 0) {
+      return 0;
+    }
+    if (nums.length == 1) {
+      return nums[0];
+    }
+    for (int i = 0; i < nums.length; ++i) {
+      if (nums[i] == 0) {
+        minNeg = maxPos = 0;
+        continue;
+      }
+      if (nums[i] < 0) {
+        long saved = maxPos;
+        maxPos = minNeg * nums[i];
+        minNeg = Math.min(nums[i], saved * nums[i]);
+      } else {
+        minNeg = minNeg * nums[i];
+        maxPos = Math.max(nums[i], maxPos * nums[i]);
+      }
+      ans = Math.max(ans, maxPos);
+    }
+    return (int) ans;
+  }
 }
