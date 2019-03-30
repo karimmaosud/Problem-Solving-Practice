3c722cd30470f4550cc5058cdab27e1514b14232
Karim Masoud <karim.masoud94@gmail.com>
2019-03-30 14:42:56 +0000

    Solve Queue Reconstruction by Height problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/QueueReconstructionByHeight.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/QueueReconstructionByHeight.java
@@ -1,5 +1,24 @@
 package com.mirak.leetcode.individual.medium;
 
+import java.util.*;
+
 public class QueueReconstructionByHeight {
 
+  public int[][] reconstructQueue(int[][] people) {
+    Arrays.sort(people, (int[] a, int[] b) -> b[0] - a[0] == 0 ? a[1] - b[1] : b[0] - a[0]);
+    for (int i = 0; i < people.length; ++i) {
+      int[] person = people[i];
+      int skipped = i - person[1];
+      for (int j = 0; j < skipped; ++j) {
+        swap(people, i - j, i - j - 1);
+      }
+    }
+    return people;
+  }
+
+  private void swap(int[][] people, int i, int j) {
+    int[] temp = people[i];
+    people[i] = people[j];
+    people[j] = temp;
+  }
 }
