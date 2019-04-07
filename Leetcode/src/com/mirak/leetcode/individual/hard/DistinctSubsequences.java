cb753e88788635814db48b878c26be95984e93c4
Karim Masoud <karim.masoud94@gmail.com>
2019-04-07 10:38:37 +0100

    Solve Distinct Subsequences I, II problems.

--- Leetcode/src/com/mirak/leetcode/individual/hard/DistinctSubsequences.java
+++ Leetcode/src/com/mirak/leetcode/individual/hard/DistinctSubsequences.java
@@ -2,4 +2,24 @@ package com.mirak.leetcode.individual.hard;
 
 public class DistinctSubsequences {
 
+  public static int numDistinct(String s, String t) {
+    if (s.length() == 0) {
+      return t.length() == 0 ? 1 : 0;
+    }
+
+    int[][] dp = new int[t.length()][s.length()];
+    for (int i = 0; i < t.length(); ++i) {
+      for (int j = 0; j < s.length(); ++j) {
+
+        if (j >= i && t.charAt(i) == s.charAt(j)) {
+          dp[i][j] = i - 1 >= 0 ? dp[i - 1][j - 1] : 1;
+        }
+
+        if (j - 1 >= 0) {
+          dp[i][j] += dp[i][j - 1];
+        }
+      }
+    }
+    return dp[t.length() - 1][s.length() - 1];
+  }
 }
--- Leetcode/src/com/mirak/leetcode/individual/hard/DistinctSubsequencesII.java
+++ Leetcode/src/com/mirak/leetcode/individual/hard/DistinctSubsequencesII.java
@@ -1,5 +1,45 @@
 package com.mirak.leetcode.individual.hard;
 
+import java.util.*;
+
 public class DistinctSubsequencesII {
+
+  private final int MOD = 1000000007;
+
+  public int distinctSubseqII(String S) {
+    int[] dp = new int[S.length()];
+    int[] lastIndex = new int[26];
+    Arrays.fill(lastIndex, -1);
+
+    for (int i = 0; i < S.length(); ++i) {
+      dp[i] = lastIndex[S.charAt(i) - 'a'] == -1 ? 1 : 0;
+      if (i - 1 >= 0) {
+        dp[i] = (dp[i] + (dp[i - 1] % MOD * 2 % MOD) % MOD) % MOD;
+      }
+      if (lastIndex[S.charAt(i) - 'a'] - 1 >= 0) {
+        dp[i] = (dp[i] - dp[lastIndex[S.charAt(i) - 'a'] - 1] + MOD) % MOD;
+      }
+      lastIndex[S.charAt(i) - 'a'] = i;
+    }
+    return dp[S.length() - 1];
+  }
+
+  // recursive solution. O(n * 26).
+  private int count(int i, String s, int[] dp) {
+    if (i == s.length()) {
+      return 0;
+    }
+    if (dp[i] != -1) {
+      return dp[i];
+    }
+    int ans = 0;
+    for (char a = 'a'; a <= 'z'; ++a) {
+      int next = s.indexOf(a, i);
+      if (next != -1) {
+        ans = (ans % MOD + (1 + count(next + 1, s, dp)) % MOD) % MOD;
+      }
+    }
+    return dp[i] = ans;
+  }
 
 }
