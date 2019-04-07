f3a420e6d390cb134b9816fe3a599c9ee4ca6495
Karim Masoud <karim.masoud94@gmail.com>
2019-04-07 10:40:26 +0100

    Solve Keys Keyboard problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/KeysKeyboard.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/KeysKeyboard.java
@@ -1,5 +1,20 @@
 package com.mirak.leetcode.individual.medium;
 
 public class KeysKeyboard {
+  public int maxA(int N) {
+    if (N == 0) {
+      return 0;
+    }
+
+    int[] dp = new int[N + 1];
+    dp[1] = 1;
+    for (int i = 2; i <= N; ++i) {
+      dp[i] = dp[i - 1] + 1;
+      for (int j = i - 3; j >= 0; --j) {
+        dp[i] = Math.max(dp[i], dp[j] * (i - j - 1));
+      }
+    }
+    return dp[N];
+  }
 
 }
