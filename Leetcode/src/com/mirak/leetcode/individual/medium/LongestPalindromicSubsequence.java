f63b24a337b07dbd688c276e6b85ac440a6ac1d5
Karim Masoud <karim.masoud94@gmail.com>
2019-04-07 10:40:54 +0100

    Solve Longest Palindromic Subsequence problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/LongestPalindromicSubsequence.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/LongestPalindromicSubsequence.java
@@ -2,4 +2,20 @@ package com.mirak.leetcode.individual.medium;
 
 public class LongestPalindromicSubsequence {
 
+  public int longestPalindromeSubseq(String s) {
+    int n = s.length();
+    String s2 = new StringBuilder(s).reverse().toString();
+    int[][] dp = new int[n + 1][n + 1];
+    for (int i = n - 1; i >= 0; --i) {
+      for (int j = n - 1; j >= 0; --j) {
+        if (s.charAt(i) == s2.charAt(j)) {
+          dp[i][j] = 1 + dp[i + 1][j + 1];
+        } else {
+          dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+        }
+      }
+    }
+    return dp[0][0];
+  }
+
 }
