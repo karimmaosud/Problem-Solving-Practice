883cccc27edb27fe00b793f5986d952faca7c645
Karim Masoud <karim.masoud94@gmail.com>
2019-04-07 17:23:45 +0100

    Solve Redundant Connection II problem.

--- Leetcode/src/com/mirak/leetcode/individual/hard/RedundantConnectionII.java
+++ Leetcode/src/com/mirak/leetcode/individual/hard/RedundantConnectionII.java
@@ -1,107 +1,111 @@
 package com.mirak.leetcode.individual.hard;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class RedundantConnectionII {
 
+  private final int SHIFT = 10;
+
   public int[] findRedundantDirectedConnection(int[][] edges) {
-
     int n = edges.length;
-    int[][] order = new int[n + 1][n + 1];
-
-    ArrayList<Integer>[] parent = new ArrayList[n + 1];
-    initList(parent);
     ArrayList<Integer>[] adjList = new ArrayList[n + 1];
-    initList(adjList);
-    initAdjList(edges, adjList, order, parent);
-    boolean[] vis = new boolean[n + 1];
-    int startNode = isCycleFromRoot(parent);
-    if (startNode == -1) {
-      // every node has only one parent, cycle must have gone through root.
-      // start with any node (because every node goes back to root through parent), and go until you find an already visited node.
-      startNode = 1;
-      while (!vis[startNode]) {
-        vis[startNode] = true;
-        startNode = parent[startNode].get(0);
-      }
-      return edgeFromCycle(parent, order, startNode);
+    ArrayList<Integer>[] parents = new ArrayList[n + 1];
+    for (int i = 0; i <= n; ++i) {
+      adjList[i] = new ArrayList<>();
+      parents[i] = new ArrayList<>();
     }
-    int[] removedEdge = new int[2];
-    dfs(startNode, adjList, vis);
-    int p1 = parent[startNode].get(0);
-    int p2 = parent[startNode].get(1);
-    if (vis[startNode]) {
-      // there must be a cycle, because you have got back to startNode
-      setEdge(removedEdge, vis[p1] ? p1 : p2, startNode);
-    } else {
-      setEdge(removedEdge, order[p1][startNode] > order[p2][startNode] ? p1 : p2, startNode);
+    init(edges, adjList, parents);
+    Map<Integer, Integer> edgeRank = getEdgeRank(edges);
+
+    int multiParentNode = getMultiParentNode(parents);
+    if (multiParentNode != -1) {
+      int[] edge1 = new int[]{parents[multiParentNode].get(0), multiParentNode};
+      int[] edge2 = new int[]{parents[multiParentNode].get(1), multiParentNode};
+      int n1 = dfs(-1, getRoot(parents), adjList, edge1);
+      int n2 = dfs(-1, getRoot(parents), adjList, edge2);
+
+      if (n1 < n) {
+        return edge2;
+      }
+      if (n2 < n) {
+        return edge1;
+      }
+      return edgeRank.get(encodeEdge(edge1)) > edgeRank.get(encodeEdge(edge2)) ? edge1 : edge2;
+    }
+    int[] cycleEdge = getCycleEdge(parents);
+    int rank = edgeRank.get(encodeEdge(cycleEdge));
+    int[] removedEdge = cycleEdge, runnerEdge = cycleEdge;
+
+    while (!Arrays.equals((runnerEdge = getNext(runnerEdge, parents)), cycleEdge)) {
+      if (edgeRank.get(encodeEdge(runnerEdge)) > rank) {
+        rank = edgeRank.get(encodeEdge(runnerEdge));
+        removedEdge = runnerEdge;
+      }
     }
     return removedEdge;
   }
 
-  private void initList(ArrayList<Integer>[] list) {
-    for (int i = 0; i < list.length; i++) {
-      list[i] = new ArrayList<>();
-    }
-  }
-
-  private void initAdjList(int[][] edges, ArrayList<Integer>[] adjList, int[][] order,
-      ArrayList<Integer>[] parent) {
-    for (int i = 0; i < edges.length; i++) {
-      int[] edge = edges[i];
-      order[edge[0]][edge[1]] = i;
+  private void init(int[][] edges, ArrayList<Integer>[] adjList,
+      ArrayList<Integer>[] parents) {
+    for (int[] edge : edges) {
       adjList[edge[0]].add(edge[1]);
-      parent[edge[1]].add(edge[0]);
+      parents[edge[1]].add(edge[0]);
     }
   }
 
-  private int isCycleFromRoot(ArrayList<Integer>[] parent) {
-    int node = -1;
-    for (int i = 0; i < parent.length; i++) {
-      if (parent[i].size() == 2) {
+  private int getMultiParentNode(ArrayList<Integer>[] parents) {
+    for (int i = 1; i < parents.length; ++i) {
+      if (parents[i].size() > 1) {
         return i;
       }
     }
-    return node;
+    return -1;
   }
 
-  private void dfs(int node, ArrayList<Integer>[] adjList, boolean[] vis) {
-    for (int i = 0; i < adjList[node].size(); i++) {
-      int v = adjList[node].get(i);
-      if (!vis[v]) {
-        vis[v] = true;
-        dfs(v, adjList, vis);
+  private int getRoot(ArrayList<Integer>[] parents) {
+    for (int i = 1; i < parents.length; ++i) {
+      if (parents[i].isEmpty()) {
+        return i;
       }
     }
+    return -1;
   }
 
-  private int[] edgeFromCycle(ArrayList<Integer>[] parent, int[][] order, int startNode) {
-
-    int[] removedEdge = new int[2];
-
-    int start = startNode;
-    int p = parent[start].get(0);
-
-    int maxOrder = -1;
-    while (p != startNode) {
-      // p -> start
-      if (order[p][start] > maxOrder) {
-        maxOrder = order[p][start];
-        setEdge(removedEdge, p, start);
+  private int dfs(int prev, int node, ArrayList<Integer>[] adjList, int[] cutEdge) {
+    int count = 1;
+    for (int v : adjList[node]) {
+      if (prev == v || (node == cutEdge[0] && v == cutEdge[1])) {
+        continue;
       }
-      start = p;
-      p = parent[start].get(0);
+      count += dfs(node, v, adjList, cutEdge);
     }
-    // p = startNode, look at last edge.
-    if (order[p][start] > maxOrder) {
-      setEdge(removedEdge, p, start);
-    }
-    return removedEdge;
+    return count;
   }
 
-  private void setEdge(int[] edge, int from, int to) {
-    edge[0] = from;
-    edge[1] = to;
+  private Map<Integer, Integer> getEdgeRank(int[][] edges) {
+    Map<Integer, Integer> edgeRank = new HashMap<>();
+    for (int i = 0; i < edges.length; ++i) {
+      edgeRank.put(encodeEdge(edges[i]), i);
+    }
+    return edgeRank;
   }
 
+  private int encodeEdge(int[] edge) {
+    return (edge[0] << SHIFT) | edge[1];
+  }
+
+  private int[] getCycleEdge(ArrayList<Integer>[] parents) {
+    int node = 1;
+    boolean[] vis = new boolean[parents.length];
+    vis[1] = true;
+    while (!vis[parents[node].get(0)]) {
+      node = parents[node].get(0);
+      vis[node] = true;
+    }
+    return new int[]{parents[node].get(0), node};
+  }
+
+  private int[] getNext(int[] edge, ArrayList<Integer>[] parents) {
+    return new int[]{parents[edge[0]].get(0), edge[0]};
+  }
 }
