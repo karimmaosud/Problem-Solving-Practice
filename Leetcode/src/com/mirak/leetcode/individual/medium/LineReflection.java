69c3c1ecd2911af011361d51d5f92ead517e2f62
Karim Masoud <karim.masoud94@gmail.com>
2019-04-07 10:36:22 +0100

    Solve Line Reflection problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/LineReflection.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/LineReflection.java
@@ -1,5 +1,39 @@
 package com.mirak.leetcode.individual.medium;
 
+import java.awt.Point;
+import java.util.*;
+
 public class LineReflection {
 
+  public boolean isReflected(int[][] points) {
+
+    Set<Point> set = new HashSet<>();
+    for (int[] point : points) {
+      set.add(new Point(point[0], point[1]));
+    }
+
+    if (set.isEmpty()) {
+      return true;
+    }
+    int midPoint = getMidPoint(set);
+    for (Point p : set) {
+      if (!set.contains(new Point(midPoint - p.x, p.y))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private int getMidPoint(Set<Point> set) {
+    int y = set.iterator().next().y;
+    int minX = Integer.MAX_VALUE;
+    int maxX = Integer.MIN_VALUE;
+    for (Point p : set) {
+      if (p.y == y) {
+        minX = Math.min(minX, p.x);
+        maxX = Math.max(maxX, p.x);
+      }
+    }
+    return minX + maxX;
+  }
 }
