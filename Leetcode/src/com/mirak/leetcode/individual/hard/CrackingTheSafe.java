c02520ad7ecc38490c51500990f39c6efb4f6313
Karim Masoud <karim.masoud94@gmail.com>
2019-04-09 12:53:19 +0100

    Solve Cracking The Safe problem.

--- Leetcode/src/com/mirak/leetcode/individual/hard/CrackingTheSafe.java
+++ Leetcode/src/com/mirak/leetcode/individual/hard/CrackingTheSafe.java
@@ -5,6 +5,10 @@ import java.util.*;
 public class CrackingTheSafe {
 
   public String crackSafe(int n, int k) {
+    return crackSafeEuler(n, k);
+  }
+
+  private String crackSafeGreedy(int n, int k) {
     Map<String, Integer> map = new HashMap<>();
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < n; i++) {
@@ -23,4 +27,48 @@ public class CrackingTheSafe {
     map.put(prev, (map.getOrDefault(prev, 1) + 1) % k);
     generateString(builder, n, k, map);
   }
+
+  public String crackSafeEuler(int n, int k) {
+    StringBuilder res = new StringBuilder();
+    if (n == 1) {
+      for (int i = 0; i < k; ++i) {
+        res.append(i);
+      }
+      return res.toString();
+    }
+    StringBuilder start = new StringBuilder();
+    for (int i = 0; i < n - 1; ++i) {
+      start.append('0');
+    }
+    Stack<String> stack = new Stack<>();
+    Map<String, Integer> nodeMask = new HashMap<>();
+    LinkedList<String> list = new LinkedList<>();
+    stack.push(start.toString());
+
+    while (!stack.isEmpty()) {
+      String current = stack.peek();
+      int mask = nodeMask.getOrDefault(current, 0);
+      if (mask == (1 << k) - 1) {
+        stack.pop();
+        list.addLast(current);
+        continue;
+      }
+      int next = getUnvisitedEdge(mask);
+      String nextState = current.substring(1) + next;
+      stack.push(nextState);
+      nodeMask.put(current, mask | (1 << next));
+    }
+
+    res.append(list.removeLast());
+    while (!list.isEmpty()) {
+      res.append(list.removeLast().charAt(n - 2));
+    }
+    return res.toString();
+  }
+
+  private static int getUnvisitedEdge(int mask) {
+    mask++;
+    return (int) (Math.log10(1.0 * (mask & -mask)) / Math.log10(2.0));
+  }
+
 }
