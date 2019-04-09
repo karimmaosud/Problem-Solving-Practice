194de6e3fa521f761bc580309c646d603dad4fbd
Karim Masoud <karim.masoud94@gmail.com>
2019-04-09 12:54:11 +0100

    Solve Remove Duplicates from Sorted Array II problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/RemoveDuplicatesFromSortedArrayII.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/RemoveDuplicatesFromSortedArrayII.java
@@ -1,5 +1,34 @@
 package com.mirak.leetcode.individual.medium;
 
 public class RemoveDuplicatesFromSortedArrayII {
+  public int removeDuplicates(int[] nums) {
+    int n = nums.length;
+    if (n == 0) {
+      return 0;
+    }
+    int idx = 0;
+    for (int i = 1; i < n; ++i) {
+      if (equalityCount(nums[i], nums, idx) > 1) {
+        continue;
+      }
+      swap(i, ++idx, nums);
+    }
+    return idx + 1;
+  }
 
+  private void swap(int i, int j, int[] nums) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
+
+  private int equalityCount(int num, int[] nums, int idx) {
+    if (idx < 1 || num != nums[idx]) {
+      return 0;
+    }
+    if (num == nums[idx - 1]) {
+      return 2;
+    }
+    return 1;
+  }
 }
