10b07fcee873f1aa2f0a5436990b5719ddbb1b7f
Karim Masoud <karim.masoud94@gmail.com>
2019-04-09 19:20:19 +0100

    Solve Flatten a Multilevel Doubly Linked List problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/FlattenAMultilevelDoublyLinkedList.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/FlattenAMultilevelDoublyLinkedList.java
@@ -2,4 +2,35 @@ package com.mirak.leetcode.individual.medium;
 
 public class FlattenAMultilevelDoublyLinkedList {
 
+  private class Node {
+
+    Node next, prev, child;
+  }
+
+  public Node flatten(Node head) {
+    Node current = head;
+    while (current != null) {
+      if (current.child == null) {
+        current = current.next;
+        continue;
+      }
+      Node moved = current.child;
+      while (moved.next != null) {
+        moved = moved.next;
+      }
+
+      Node temp = current.next;
+      current.next = current.child;
+      current.next.prev = current;
+      current.child = null;
+      if (temp != null) {
+        moved.next = temp;
+        temp.prev = moved;
+      }
+
+      current = current.next;
+    }
+    return head;
+  }
+
 }
