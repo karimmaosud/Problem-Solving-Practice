c7e6283b4c96aa8f747ec4563ffb84bfacb3040b
Karim Masoud <karim.masoud94@gmail.com>
2019-04-28 07:56:28 +0100

    Solve Champagne Tower problem.

--- Leetcode/src/com/mirak/leetcode/individual/medium/ChampagneTower.java
+++ Leetcode/src/com/mirak/leetcode/individual/medium/ChampagneTower.java
@@ -2,4 +2,23 @@ package com.mirak.leetcode.individual.medium;
 
 public class ChampagneTower {
 
+  public double champagneTower(int poured, int query_row, int query_glass) {
+    double[][] glasses = new double[100][100];
+    glasses[0][0] = poured;
+    for (int i = 0; i < glasses.length; ++i) {
+      for (int j = 0; j < glasses[i].length; ++j) {
+        if (glasses[i][j] > 1) {
+          double excess = glasses[i][j] - 1;
+          if (i + 1 < glasses.length) {
+            glasses[i + 1][j] += excess / 2.0;
+            if (j + 1 < glasses[i + 1].length) {
+              glasses[i + 1][j + 1] += excess / 2.0;
+            }
+          }
+          glasses[i][j] = 1.0;
+        }
+      }
+    }
+    return glasses[query_row][query_glass];
+  }
 }
